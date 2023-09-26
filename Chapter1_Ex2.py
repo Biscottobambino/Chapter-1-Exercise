d06511d32573ff51133bb17c2420221ed34b91de
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-09-26 01:40:42 -0700

    Update Chapter1_Ex2.py

--- Chapter1_Ex2.py
+++ Chapter1_Ex2.py
@@ -1,3 +1,7 @@
+"""
+Chapter 1 : Exercise 2 | Print the version of python
+Write a Python program to get the Python version you are using.
+"""
 import sys
 print('python version')
 print(sys.version)
