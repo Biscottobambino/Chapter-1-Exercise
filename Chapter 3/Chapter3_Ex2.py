21601990cddca8037271e34c6afed37cea70ffb9
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:12:26 +0400

    Update Chapter3_Ex2.py

--- Chapter 3/Chapter3_Ex2.py
+++ Chapter 3/Chapter3_Ex2.py
@@ -1,11 +1,12 @@
 """
-CHAPTER 3, EXERCISE 1 - NAMES 
+CHAPTER 3, EXERCISE 1 - GREETINGS 
 
 Start with the list you used in Exercise 1, but instead of just
 
 printing each person’s name, print a message to them. The text of each message should be the same, but each message should be
 
 personalized with the person’s name.
+
 
 """
 #put values in 'names's and print for the response
