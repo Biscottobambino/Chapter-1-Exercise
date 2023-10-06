6c76c75560b35aab86922325e01e665b597ff2be
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:23:44 +0400

    Update Chapter1_Ex3.py

--- Chapter 1/Chapter1_Ex3.py
+++ Chapter 1/Chapter1_Ex3.py
@@ -1,5 +1,6 @@
 """
-Chapter 1 Exercise 3 | Print date and time
+CHAPTER 1, EXERCISE 3 - PRINT DATE AND TIME 
+
 Write a Python program to display the current date and time.
 """
 
@@ -7,3 +8,11 @@ import datetime
 now = datetime.datetime.now()
 print ('current date and time')
 print(now.strftime("%Y-%m-d%H:%M:S"))
+
+"""
+Output:
+
+current date and time
+2023-10-d14:23:S
+
+"""
