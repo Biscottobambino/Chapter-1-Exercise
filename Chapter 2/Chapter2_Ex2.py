34683c72476688c8d967569010a477801a8999ab
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:18:54 +0400

    Update Chapter2_Ex2.py

--- Chapter 2/Chapter2_Ex2.py
+++ Chapter 2/Chapter2_Ex2.py
@@ -1,5 +1,6 @@
 """
-Chapter 2 Exercise 2:
+CHAPTER 2, EXERCISE 2 - VARIABLES 
+
 Find a quote from a famous person you admire. Print the quote and the name of its author.
 Your output should look something like the following, including the quotation marks: 
 Albert Einstein once said, “A person who never made a mistake never tried anything new.
@@ -8,3 +9,11 @@ Albert Einstein once said, “A person who never made a mistake never tried anyt
 
 print('Master Yoda once said, "Do or do not')
 print('there is no try."')
+
+"""
+Output: 
+Master Yoda once said, "Do or do not
+there is no try."
+
+"""
+
