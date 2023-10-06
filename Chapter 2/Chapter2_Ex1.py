3e97d39c683d32b4f0a907494e4c0afda99c7052
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:19:57 +0400

    Update Chapter2_Ex1.py

--- Chapter 2/Chapter2_Ex1.py
+++ Chapter 2/Chapter2_Ex1.py
@@ -1,5 +1,6 @@
 """
-Chapter 2 Exercise 1
+CHAPTER 2, EXERCISE 1 
+
 Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
 
 """
@@ -8,3 +9,11 @@ a=(input("Enter your name = "))
 b=(input("Enter your age ="))
 print("Name - ", a)
 print("Age - ", b)
+
+""" 
+Output: 
+Enter your name = Ken
+Enter your age =17
+Name -  Ken
+Age -  17
+"""
