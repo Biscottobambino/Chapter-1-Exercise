e877d200029088fec2e977f8bc2028860dcf75cd
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:05:05 +0400

    Update Chapter3_Ex3.py

--- Chapter 3/Chapter3_Ex3.py
+++ Chapter 3/Chapter3_Ex3.py
@@ -9,3 +9,14 @@ to print a series of statements about these items, such as “I would like to ow
 JDMCar_models = ['Honda NSX', 'Mazda Mx5', 'Nissan R34 Skyline', 'AE86', 'Toyoya Stupra']
 for JDMCar_model in JDMCar_models:
  print(f"I'd love to have a {JDMCar_model} one day!")
+
+#Output 
+"""
+I'd love to have a Honda NSX one day!
+I'd love to have a Mazda Mx5 one day!
+I'd love to have a Nissan R34 Skyline one day!
+I'd love to have a AE86 one day!
+I'd love to have a Toyoya Stupra one day!
+ 
+
+"""
