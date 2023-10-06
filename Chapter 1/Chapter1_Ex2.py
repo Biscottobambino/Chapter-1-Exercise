59373327c6452bcb2d3b5dfbd49f1ad687a67801
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:22:35 +0400

    Update Chapter1_Ex2.py

--- Chapter 1/Chapter1_Ex2.py
+++ Chapter 1/Chapter1_Ex2.py
@@ -1,5 +1,5 @@
 """
-Chapter 1 : Exercise 2 | Print the version of python
+CHAPTER 1, EXERCISE 2 - PRINT THE VERSION OF PYTHON 
 Write a Python program to get the Python version you are using.
 """
 import sys
@@ -8,3 +8,11 @@ print(sys.version)
 print('version info')
 print(sys.version_info)
 
+"""
+Output: 
+
+python version
+3.10.11 (main, Apr  4 2023, 22:10:32) [GCC 12.2.0]
+version info
+sys.version_info(major=3, minor=10, micro=11, releaselevel='final', serial=0)
+"""
