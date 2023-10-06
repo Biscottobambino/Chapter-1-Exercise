dbb5a0a8ef6824cf5c0d3213dca6e780b240f3ce
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:07:51 +0400

    Update Chapter3_Ex4.py

--- Chapter 3/Chapter3_Ex4.py
+++ Chapter 3/Chapter3_Ex4.py
@@ -1,4 +1,10 @@
+"""
+CHAPTER 3, EXERCISE 4 - GUEST LIST 
 
+If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
+
+like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner.
+"""
 guests = ['Darrel Tan', 'Nathan Bartolo', 'Kenlee Labordo']
 
 name = guests[0].title()
@@ -9,3 +15,13 @@ print(f"{name}, welcome to the party.")
 
 name = guests[2].title()
 print(f"{name}, welcome to the party.")
+
+"""
+Output:
+
+Darrel Tan, welcome to the party.
+Nathan Bartolo, welcome to the party.
+Kenlee Labordo, welcome to the party.
+ 
+
+"""
