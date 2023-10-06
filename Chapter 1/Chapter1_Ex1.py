40179f4e78f85ec9595d1f675a994f163754c771
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:21:43 +0400

    Update Chapter1_Ex1.py

--- Chapter 1/Chapter1_Ex1.py
+++ Chapter 1/Chapter1_Ex1.py
@@ -1,5 +1,5 @@
 """
-Chapter 1 Exercise 1
+CHAPTER 1, EXERCISE 1 - PRINT STRINGS 
 
 Write a Python program to print the following string in a specific format.
 Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are
@@ -7,3 +7,14 @@ Twinkle, twinkle, little star, How I wonder what you are! Up above the world so 
 """"
 
 print('Twinkle twinkle little star, \n\t How I wonder what you are! \n\t\t up above the world so high, \n\t\t Like a diamond in the sky!, \n Twinkle, twinkle little star, \n\t how I wonder what you are!')
+
+"""
+Output: 
+
+Twinkle twinkle little star, 
+     How I wonder what you are! 
+         up above the world so high, 
+         Like a diamond in the sky!, 
+ Twinkle, twinkle little star, 
+     how I wonder what you are!
+"""
