1839417f175a56fcdf6a24b99091b59f9c85ee5e
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:24:48 +0400

    Update Chapter1_Ex4.py

--- Chapter 1/Chapter1_Ex4.py
+++ Chapter 1/Chapter1_Ex4.py
@@ -1,5 +1,5 @@
 """
-Chapter 1 Exercise 4 | Strings Concatination
+CHAPTER 1, EXERCISE 4 - STRING CONCATINATION 
 
 Write three strings in different variables and print the output as one string.      
 
@@ -9,3 +9,9 @@ a= 23
 b= 24
 c= 25
 print(a,b,c)
+
+"""
+Output: 
+
+23 24 25
+"""
