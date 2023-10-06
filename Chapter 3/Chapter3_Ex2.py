834237679d6b4b5e105c96ad7f0e0e1ce73f92de
Kenlee Labordo <144359480+Biscottobambino@users.noreply.github.com>
2023-10-06 18:04:09 +0400

    Update Chapter3_Ex2.py

--- Chapter 3/Chapter3_Ex2.py
+++ Chapter 3/Chapter3_Ex2.py
@@ -1,3 +1,14 @@
+"""
+CHAPTER 3, EXERCISE 1 - NAMES 
+
+Start with the list you used in Exercise 1, but instead of just
+
+printing each person’s name, print a message to them. The text of each message should be the same, but each message should be
+
+personalized with the person’s name.
+
+"""
+#put values in 'names's and print for the response
 names = ['Darrel', 'Nathan', 'Kenlee', 'Bayla', 'Yang']
 for name in names:
  print(f"Hey {name}! Have a lovely day")
