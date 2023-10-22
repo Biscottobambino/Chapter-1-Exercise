1be7fcc758bb494e825e275079978e2990f33336
Kenlee Labordo <144359480+KenleeLabordo@users.noreply.github.com>
2023-10-22 19:09:17 +0400

    Update Chapter1_Ex1.py

--- Chapter 1/Chapter1_Ex1.py
+++ Chapter 1/Chapter1_Ex1.py
@@ -5,7 +5,7 @@ Write a Python program to print the following string in a specific format.
 Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are
 
 """"
-
+#Using /n for the next line and /t for space. 
 print('Twinkle twinkle little star, \n\t How I wonder what you are! \n\t\t up above the world so high, \n\t\t Like a diamond in the sky!, \n Twinkle, twinkle little star, \n\t how I wonder what you are!')
 
 """
